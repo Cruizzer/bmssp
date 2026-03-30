7196303077064d0c248284ba4d241ee3c7982c01
Cruizzer <archcruizer@gmail.com>
2026-03-30 13:11:30 +0100

    Basic CMake structure

---
+++ build/CMakeFiles/bmssp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bmssp.dir/link.d"
+  "CMakeFiles/bmssp.dir/src/main.cpp.o"
+  "CMakeFiles/bmssp.dir/src/main.cpp.o.d"
+  "bmssp"
+  "bmssp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bmssp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
